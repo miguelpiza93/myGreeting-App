bf1aa03562698bf3096ead91aaad300af7a76236
unknown <mapiz_930@hotmail.com>
2017-04-12 17:35:22 -0500

    commit

---
+++ src/main/java/com/apus/greeting/MyGreetingAppApplication.java
@@ -0,0 +1,12 @@
+package com.apus.greeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGreetingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGreetingAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/apus/greeting/MyGreetingAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.apus.greeting;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyGreetingAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
